e3c21404e0f1a40bd4b2e14c3eb03001b4170f4d
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-09-26 14:53:22 +0300

    Delete 6kyu_duplicate_encode.py

--- 6kyu_duplicate_encode.py
+++
@@ -1,7 +0,0 @@
-# "recede"   =>  "()()()"
-def duplicate_encode(word):
-    from collections import Counter
-    counter = Counter(word)
-    return "".join(")" if counter[i] > 1 else "(" for i in word)
-
-print(duplicate_encode('(( @'))
