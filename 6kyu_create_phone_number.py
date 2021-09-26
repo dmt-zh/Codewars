cec43f2797883a17fedc39a500615ce1296138df
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-09-26 14:53:01 +0300

    Delete 6kyu_create_phone_number.py

--- 6kyu_create_phone_number.py
+++
@@ -1,5 +0,0 @@
-# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) => returns "(123) 456-7890"
-def create_phone_number(n):
-    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
-    
-print(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
