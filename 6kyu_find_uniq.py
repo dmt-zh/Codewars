27c814f031d0031000d57be9686a854f584bcf88
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-09-26 14:53:42 +0300

    Delete 6kyu_find_uniq.py

--- 6kyu_find_uniq.py
+++
@@ -1,9 +0,0 @@
-# There is an array with some numbers. All numbers are equal except for one. Try to find it.
-# find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
-
-def find_uniq(arr):
-    from collections import Counter
-    counter = Counter(arr)
-    return counter.most_common()[:-2:-1][0][0]
-
-print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
