77f4588961fe541134e4f2ea1dc8fa12cdf9342c
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-10-13 22:38:06 +0300

    Add files via upload

---
+++ 7 kyu/7kyu_decode_pass.py
@@ -0,0 +1,17 @@
+# A wealthy client has forgotten the password to his business website, but he has a list of possible
+# passwords. His previous developer has left a file on the server with the name password.txt. You open
+# the  file and realize it's in binary format.
+
+# Write a script that takes an array of possible passwords and a string of binary representing the possible
+# password. Convert the binary to a string and compare to the password array. If the password is found, return
+# the password string, else return false;
+
+# decode_pass(['password123', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => 'password123'
+# decode_pass(['password321', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => False
+
+def decode_pass(pass_list, bits):
+    psw = "".join([chr(int(binary, 2)) for binary in bits.split()])
+    return psw if psw in pass_list else False
+
+
+
