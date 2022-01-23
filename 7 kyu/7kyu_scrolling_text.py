a3fef39c3a0dfc27ce2fb560048fb3c2452ae671
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2022-01-23 19:24:15 +0300

    Add files via upload

---
+++ 7 kyu/7kyu_scrolling_text.py
@@ -0,0 +1,19 @@
+# Let's create some scrolling text! Your task is to complete the function
+# which takes a string, and returns an array with all possible rotations of
+# the given string, in uppercase.
+
+# Example
+# scrollingText("codewars") should return:
+
+# [ "CODEWARS",
+#   "ODEWARSC",
+#   "DEWARSCO",
+#   "EWARSCOD",
+#   "WARSCODE",
+#   "ARSCODEW"
+#   "RSCODEWA",
+#   "SCODEWAR" ]
+
+
+def scrolling_text(text):
+    return [''.join([text.upper()[i:], text.upper()[:i]]) for i in range(len(text))]
