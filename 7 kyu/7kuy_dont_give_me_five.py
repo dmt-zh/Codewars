f66393fcb4a316b08c81f02439764c829f9952f9
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2022-05-19 22:11:26 +0300

    Add files via upload

---
+++ 7 kyu/7kuy_dont_give_me_five.py
@@ -0,0 +1,11 @@
+# Don't give me five!
+# In this kata you get the start number and the end number of a region and should return the count
+# of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+# Examples:
+# 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+# 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+def dont_give_me_five(start, end):
+    n = [i for i in range(start, end + 1) if '5' not in str(i)]
+    return len(n)
