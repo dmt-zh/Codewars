55b57b28680d0cad96022d1c23315c0ccb93d8fd
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2022-08-29 18:24:28 +0300

    Add files via upload

---
+++ JavaScript/8kyu_MrFreeze.js
@@ -0,0 +1,9 @@
+// There is an object/class already created called MrFreeze. 
+// Mark this object as frozen so that no other changes can be made to it.
+
+
+function freezeObj(obj){
+    Object.freeze(obj);
+}
+
+freezeObj(MrFreeze);
